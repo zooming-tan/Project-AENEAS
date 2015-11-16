2f8c017d2ebc31fe2c3c1e16ceee1be9a65fb19d
zu.ming.tan <zooming.tan@gmail.com>
2015-11-16 16:34:49 +0800

    Delete project field

--- Project-AENEAS/issues/models.py
+++ Project-AENEAS/issues/models.py
@@ -3,7 +3,6 @@ http://news.e-scribe.com/230 and snippet: http://djangosnippets.org/snippets/28/
 """
 from django.db import models
 from django.contrib.auth.models import User
-from django.conf import settings
 from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 
@@ -20,14 +19,11 @@ PRIORITY_CODES = (
     (3, _('Someday')),
 )
 
-apps = [app for app in settings.INSTALLED_APPS if not app.startswith('django.')]
-
 
 @python_2_unicode_compatible
 class Ticket(models.Model):
     """Trouble tickets"""
     title = models.CharField(_('title'), max_length=100)
-    project = models.CharField(_('project'), blank=True, max_length=100, choices=list(enumerate(apps)))
     submitted_date = models.DateField(_('date submitted'), auto_now_add=True)
     modified_date = models.DateField(_('date modified'), auto_now=True)
     submitter = models.ForeignKey(User, verbose_name=_('submitter'), related_name="submitter")
