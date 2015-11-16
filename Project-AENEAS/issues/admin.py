9bbfcda71349d9ce544c5783e5e9b2b4bb62d9d2
zu.ming.tan <zooming.tan@gmail.com>
2015-11-16 17:54:54 +0800

    Register Ticket model to the admin interface

--- Project-AENEAS/issues/admin.py
+++ Project-AENEAS/issues/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Ticket
 
-# Register your models here.
+
+class TicketAdmin(admin.ModelAdmin):
+    #  https://docs.djangoproject.com/en/1.8/intro/tutorial02/
+    exclude = ('submitted_date', 'modified_date',)
+    list_display = ('submitter', 'description', 'submitted_date')
+
+
+admin.site.register(Ticket, TicketAdmin)
