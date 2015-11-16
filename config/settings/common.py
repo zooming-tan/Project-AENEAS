e1a9160217a3b522367974aa6228086d819a7a1c
zu.ming.tan <zooming.tan@gmail.com>
2015-11-16 17:29:54 +0800

    Add issue app

--- config/settings/common.py
+++ config/settings/common.py
@@ -44,6 +44,7 @@ THIRD_PARTY_APPS = (
 # Apps specific for this project go here.
 LOCAL_APPS = (
     'Project-AENEAS.users',  # custom users app
+    'Project-AENEAS.issues',
     # Your stuff: custom apps go here
 )
 
